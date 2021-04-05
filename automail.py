a0f52dd3870b4ca6007c3642e95bb9e92bce6230
Saubhagya Ranjan Jena <63902095+rRiks@users.noreply.github.com>
2021-04-06 01:19:11 +0530

    Update automail.py

--- automail.py
+++ automail.py
@@ -11,13 +11,13 @@ files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
 newest = files[-1]
 filename = newest
 sub = "Attendance Report for " + str(date)
-user_mail = "riks.rs18@gmail.com"
+user_mail = "*********@gmail.com"
 #user_password = input("enter user {}'s password:".format(user_mail))
-user_password = "Google001"
+user_password = "***********"
 yag = yagmail.SMTP(user=user_mail, password=user_password)
-#yag = yagmail.SMTP("riks.rs18@gmail.com", oauth2_file="~/oauth2_creds.json")
-# recipients = ['tafiquemd@gmail.com','sanskruti324@gmail.com']
-recipients = ['tafiquemd@gmail.com']
+#yag = yagmail.SMTP("*********@gmail.com", oauth2_file="~/oauth2_creds.json")
+# recipients = ['*********@gmail.com','*********@gmail.com']
+recipients = ['*********@gmail.com']
 #recipients = input("Enter recivier's mail id: ").split(",")
 yag.send(to=recipients,subject=sub,attachments= filename)
 print("Email Sent!")
